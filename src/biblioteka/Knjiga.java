6b5005baa71b42d9a78ff17ec5e18e96b417386b
vukdjedovic <vukdjedovic98@gmail.com>
2022-03-02 09:58:05 +0100

    Dodati atributi i get i set metode u klasu Knjiga

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,5 +1,10 @@
 package biblioteka;
 
 public class Knjiga {
-
+	
+     private String naslov;
+     private long isbn;
+     private String autori;
+     private String izdavac;
+     private int izdanje;
 }
