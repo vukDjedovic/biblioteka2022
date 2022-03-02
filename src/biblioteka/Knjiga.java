76579b6bfa1aff3bf73ce2fdd9f6a70f0698264b
vukdjedovic <vukdjedovic98@gmail.com>
2022-03-02 10:26:55 +0100

    Refaktorisana klasa Knjiga, uvedena klasa Autor

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,10 +1,12 @@
 package biblioteka;
 
+import java.util.List;
+
 public class Knjiga {
 	
      private String naslov;
      private long isbn;
-     private String autori;
+     private List<Autor> autori;
      private String izdavac;
      private int izdanje;
      
@@ -21,10 +23,10 @@ public class Knjiga {
 	public void setIsbn(long isbn) {
 		this.isbn = isbn;
 	}
-	public String getAutori() {
+	public List<Autor> getAutori() {
 		return autori;
 	}
-	public void setAutori(String autori) {
+	public void setAutori(List<Autor> autori) {
 		this.autori = autori;
 	}
 	public String getIzdavac() {
